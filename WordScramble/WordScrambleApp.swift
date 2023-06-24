d5f4c471de0ef424485e8c64dcb93b3b0185db28
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-24 15:07:41 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Khumbongmayum Tonny on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
